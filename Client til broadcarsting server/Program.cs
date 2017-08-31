58e55ed927ff2fbfe5187b11e2a26436578a7b35
Jimmi Christensen <jimm1403@edu.eal.dk>
2017-08-31 11:41:33 +0200

    Add project files.

---
+++ Client til broadcarsting server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.Run();
+        }
+        public void Run()
+        {
+            string servername = Console.ReadLine();
+            int port = int.Parse(Console.ReadLine());
+            ServerFacade severFacade = new ServerFacade(port, servername);
+        }
+    }
+}
---
+++ Client til broadcarsting server/Server facade.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace ChatClient
+{
+    class ServerFacade
+    {
+        TcpClient client;
+        NetworkStream nWS;
+        StreamReader sR;
+        StreamWriter sW;
+        int port;
+        string servername;
+
+        public ServerFacade(int port, string servername)
+        {
+            this.port = port;
+            this.servername = servername;
+            client = new TcpClient();
+            ConnectToSever();
+            nWS = client.GetStream();
+            sR = new StreamReader(nWS);
+            sW = new StreamWriter(nWS);
+
+            Thread sendThred = new Thread(Send);
+            Thread reciveThread = new Thread(Recive);
+            reciveThread.Start();
+            sendThred.Start();
+        }
+        private void ConnectToSever()
+        {
+            client.Connect(servername, port);
+        }
+
+        private void Close()
+        {
+            client.GetStream().Close();
+        }
+
+        private void Dispose()
+        {
+            client.Close();
+        }
+
+        public void Recive()
+        {
+            string recivemessage;
+            while (true)
+            {
+                recivemessage = sR.ReadLine();
+                Console.WriteLine(recivemessage);
+            }
+        }
+
+        public void Send()
+        {
+            string message;
+            while (true)
+            {
+                message = Console.ReadLine();
+                sW.WriteLine(message);
+                sW.Flush();
+            }
+        }
+    }
+}
